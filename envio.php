4290280532e98ec4aed835ae5eae4dfc7d6bc008
deiby13_96 <deiby13_96@hotmail.com>
2021-02-26 01:03:51 -0500

    codificacion de el codigo para enviar correos con php

--- envio.php
+++ envio.php
@@ -1,24 +1,16 @@
 <?php
+
+    $destinatario = 'deibyokacoma@gmail.com';
+
     $nombre = $_POST['nombre'];
     $mail = $_POST['email'];
     $asunto = $_POST['asunto'];
     $empresa = $_POST['mensaje'];
 
-    $header = 'From: ' . $mail . " \r\n";
-    $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-    $header .= "Mime-Version: 1.0 \r\n";
-    $header .= "Content-Type: text/plain";
+    $header = "Enviado desde la pagina THE WEIGTH";
+    $mensajeCompleto = $empresa . "\nAtentamente: " . $nombre;
 
-    $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
-    $mensaje .= "Su e-mail es: " . $mail . " \r\n";
-    $mensaje .= "Asunto:" . $asunto . "\r\n";
-    $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
-    $mensaje .= "Enviado el " . date('d/m/Y', time());
-
-    $para = 'deibyokacoma@gmail.com';
-    $asunto = 'Mensaje de mi sitio web';
-
-    mail($para, $asunto, utf8_decode($mensaje), $header);
-
-    header("Location:index.html");
+    mail($destinatario, $asunto, $mensajeCompleto, $header);
+    echo "<script>alert('correo enviado exitosamente')</script>";
+    echo "<script> setTimeout(\"location.href='index.html'\", 1000)</script>";
 ?>
