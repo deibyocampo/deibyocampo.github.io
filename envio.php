6677ed328f9146fe1249b635b1344c7ca7f648d2
deiby13_96 <deiby13_96@hotmail.com>
2021-02-26 00:35:06 -0500

    cambio del boton enviar mensaje y en el archivo .php

--- envio.php
+++ envio.php
@@ -1,24 +1,24 @@
 <?php
-$nombre = $_POST['nombre'];
-$mail = $_POST['email'];
-$asunto = $_POST['asunto'];
-$empresa = $_POST['mensaje'];
+    $nombre = $_POST['nombre'];
+    $mail = $_POST['email'];
+    $asunto = $_POST['asunto'];
+    $empresa = $_POST['mensaje'];
 
-$header = 'From: ' . $mail . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+    $header = 'From: ' . $mail . " \r\n";
+    $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+    $header .= "Mime-Version: 1.0 \r\n";
+    $header .= "Content-Type: text/plain";
 
-$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
-$mensaje .= "Su e-mail es: " . $mail . " \r\n";
-$mensaje .= "Asunto:" . $asunto . "\r\n";
-$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
-$mensaje .= "Enviado el " . date('d/m/Y', time());
+    $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+    $mensaje .= "Su e-mail es: " . $mail . " \r\n";
+    $mensaje .= "Asunto:" . $asunto . "\r\n";
+    $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+    $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'deibyokacoma@gmail.com';
-$asunto = 'Mensaje de mi sitio web';
+    $para = 'deibyokacoma@gmail.com';
+    $asunto = 'Mensaje de mi sitio web';
 
-mail($para, $asunto, utf8_decode($mensaje), $header);
+    mail($para, $asunto, utf8_decode($mensaje), $header);
 
-header("Location:index.html");
+    header("Location:index.html");
 ?>
